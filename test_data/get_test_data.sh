151065588cbff14259738bc7864b36a1a7edb4f2
Kai Blumberg <kblumberg@email.arizona.edu>
2020-06-11 17:22:03 -0700

    Add Tara 454 sample data

--- test_data/get_test_data.sh
+++ test_data/get_test_data.sh
@@ -2,6 +2,10 @@
 
 set -u
 
+#OSD illumina example
 curl -O ftp.sra.ebi.ac.uk/vol1/fastq/ERR771/ERR771001/ERR771001_1.fastq.gz
 
 curl -O ftp.sra.ebi.ac.uk/vol1/fastq/ERR771/ERR771001/ERR771001_2.fastq.gz
+
+# Tara 454 example
+curl -O ftp.sra.ebi.ac.uk/vol1/fastq/ERR321/ERR321017/ERR321017.fastq.gz
